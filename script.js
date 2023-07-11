de71ad3256870c01cdb78979f481cd742bb5bf3e
Edil-Ken <edikoskyrim@gmail.com>
2023-07-11 14:22:40 +0600

    tasks

---
+++ script.js
@@ -0,0 +1,53 @@
+// let a = 'hello'
+// let b = 'Edil'
+//
+// console.log(a.slice(0, 1).toUpperCase() + a.slice(1) + ' ' + b)
+
+// let a = 'lorem ipsum dolor'
+// let b = a.length
+//
+// console.log(b)
+
+// let a = 'lorem ipsum dolor sit amet'
+//
+// console.log(a.slice(a.indexOf('sit'), a.indexOf('amet')) + a.slice(0, a.indexOf('sit')) + a.slice(a.indexOf('amet')))
+
+// let a = 'children'
+//
+// console.log(a.slice(0, a.indexOf(a[a.length - 1])).toUpperCase() + a.slice(a.length - 1))
+
+// let a = 'boss'
+//
+// console.log(a.slice(0, 1) + a.slice(2) + a.slice(1, 2))
+
+// let a = 'sunday'
+//
+// console.log(a.indexOf('d'))
+
+// let a = 'hello'
+//
+// console.log(a.replace('ll', 'ks'))
+
+// let a = 'user'
+// let b = 'new'
+//
+// console.log(b + ' ' + a.slice(0, 2))
+
+// let a = 'monday'
+// let b = 'day'
+//
+// console.log(b + b + a.slice(0, a.indexOf('d')))
+
+// let a = 'HELLO'
+//
+// console.log(a[0].toLowerCase() + a[1] + a[2].toLowerCase() + a[3] + a[4])
+
+// let a = 5
+// let b = '400'
+//
+// console.log(b.replace('0', a))
+
+// let a = 'monkey'
+// let b = 'banana'
+//
+// console.log(a.slice(0, 1) + b.slice(1, 4) + a.slice(3, 4))
